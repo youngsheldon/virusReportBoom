17e1d0e04e49766fbd908c7910728e7a7a0dc720
youngsheldon <1349739532@qq.com>
2017-03-20 16:50:07 +0800

    v9

--- shell/download_virus_sm_alarm.sh
+++ shell/download_virus_sm_alarm.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 sqlplus -s smmcadmin/AdminDB^12@SMMC <<EOF
-# set trimspool on 
-# set linesize 1024
-# set pagesize 2000 
-# set heading off 
-# set term off 
-set newp none
-set colsep ,
-set echo off
-set feedback off
-set heading off
-set pagesize 0
-set termout off
+set colsep ,  
+set feedback off  
+set heading off  
 set trimout on
-set trimspool on
-set linesize 1024
+set linesize 2048
+set newp none 
 spool data_sort/ref_data/virus_sm_alarm_$1.csv
 select code||','||recv_time||','||sm_text||','||nnos_hash||','||sm_count||','||src_addr_count||','||dst_addr_count||','||url||','||Surl||','||Eurl||','||Sip||','||target_ip||','||file_md5||','||authority||','||probability||','||alarm_time||','||alarm_grade||','||alarm_code||','||virus_flag from virus_sm_alarm_$1;
 spool off
