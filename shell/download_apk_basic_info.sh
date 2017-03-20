b0bf6dba01a9ce35ed4a259bb0bb33d9af49374f
youngsheldon <1349739532@qq.com>
2017-03-20 16:09:55 +0800

    v4

--- shell/download_apk_basic_info.sh
+++ shell/download_apk_basic_info.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 sqlplus -s smmcadmin/AdminDB^12@SMMC <<EOF
-set trimspool on 
+# set trimspool on 
+# set linesize 1024
+# set pagesize 2000 
+# set heading off 
+# set term off 
+set newp none
+set colsep ,
+set echo off
+set feedback off
+set heading off
+set pagesize 0
+set termout off
+set trimout on
+set trimspool on
 set linesize 1024
-set pagesize 2000 
-set heading off 
-set term off 
 spool data_sort/ref_data/apk_basic_info.csv 
 select code||','||name||','||MD5||','||SHA1||','||SHA256||','||SHA512||','||first_found||','||last_update||','||short_url||','||long_url||','||content||','||risk||','||category||','||cost||','||low_config||','||better_config||','||init_ip||','||init_ip_attribution||','||target_ip||','||target_ip_attribution||','||apk_file_size||','||apk_source_num  from apk_basic_info;
 spool off
