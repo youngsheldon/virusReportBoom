3912ba0598b4f37fe7028e940285c7ad48f27b19
youngsheldon <1349739532@qq.com>
2017-03-20 15:41:57 +0800

    v3

--- shell/download_virus_sm_alarm.sh
+++ shell/download_virus_sm_alarm.sh
@@ -6,6 +6,6 @@ set pagesize 2000
 set heading off 
 set term off 
 spool data_sort/ref_data/virus_sm_alarm_$1.csv
-select code||','||recv_time||','||sm_text||','||nnos_hash||','||src_addr_count||','||dst_addr_count||','||url||','||Surl||','||Eurl||','||Sip||','||target_ip||','||file_md5||','||probability||','||alarm_time||','||alarm_grade||','||alarm_code||','||virus_flag from virus_sm_alarm_$1;
+select code||','||recv_time||','||sm_text||','||nnos_hash||','||sm_count||','||src_addr_count||','||dst_addr_count||','||url||','||Surl||','||Eurl||','||Sip||','||target_ip||','||file_md5||','||authority||','||probability||','||alarm_time||','||alarm_grade||','||alarm_code||','||virus_flag from virus_sm_alarm_$1;
 spool off
 EOF
